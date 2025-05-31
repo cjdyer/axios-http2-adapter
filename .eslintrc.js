42c975556dd6c724a39d3188717e40e40ebed6cb
Cory Dyer <cjdyer0@gmail.com>
2025-05-31 15:47:26 +1000

    chore: set tab width to 4

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,10 @@ module.exports = {
     parser: '@typescript-eslint/parser',
     extends: [
         'plugin:@typescript-eslint/recommended',
-        'plugin:prettier/recommended'
+        'plugin:prettier/recommended',
     ],
     rules: {
-        'prettier/prettier': 'error'
+        'prettier/prettier': 'error',
     },
     parserOptions: {
         ecmaVersion: 2020,
--- src/adapter.test.ts
+++ src/adapter.test.ts
@@ -4,40 +4,40 @@ import { createHTTP2Adapter } from './adapter';
 import http from 'http';
 
 describe('HTTP2 Adapter for Axios', () => {
-  let axiosInstance: AxiosInstance;
-  let http1Server: http.Server;
+    let axiosInstance: AxiosInstance;
+    let http1Server: http.Server;
 
-  beforeAll(() => {
-    http1Server = createHttp1MockServer(3001);
-  });
-
-  beforeEach(() => {
-    axiosInstance = axios.create({ adapter: createHTTP2Adapter() });
-  });
-
-  afterAll(() => {
-    http1Server.close();
-  });
-
-  it('should make a successful HTTP/2 request', async () => {
-    const response = await axiosInstance.get('https://google.com');
-    expect(response.headers[':status']).toEqual('200');
-  });
-
-  it('should fallback to HTTP/1.1 if HTTP/2 is not supported', async () => {
-    const axiosInstance = axios.create({
-      adapter: createHTTP2Adapter(),
+    beforeAll(() => {
+        http1Server = createHttp1MockServer(3001);
     });
 
-    const response = await axiosInstance.get('http://localhost:3001');
-    expect(response.data).toBe('HTTP/1.1 Response');
-    expect(response.status).toBe(200);
-  });
+    beforeEach(() => {
+        axiosInstance = axios.create({ adapter: createHTTP2Adapter() });
+    });
+
+    afterAll(() => {
+        http1Server.close();
+    });
+
+    it('should make a successful HTTP/2 request', async () => {
+        const response = await axiosInstance.get('https://google.com');
+        expect(response.headers[':status']).toEqual('200');
+    });
+
+    it('should fallback to HTTP/1.1 if HTTP/2 is not supported', async () => {
+        const axiosInstance = axios.create({
+            adapter: createHTTP2Adapter(),
+        });
+
+        const response = await axiosInstance.get('http://localhost:3001');
+        expect(response.data).toBe('HTTP/1.1 Response');
+        expect(response.status).toBe(200);
+    });
 });
 
 export function createHttp1MockServer(port: number): http.Server {
-  const app = express();
-  app.get('/', (req, res) => res.send('HTTP/1.1 Response'));
+    const app = express();
+    app.get('/', (req, res) => res.send('HTTP/1.1 Response'));
 
-  return app.listen(port);
+    return app.listen(port);
 }
