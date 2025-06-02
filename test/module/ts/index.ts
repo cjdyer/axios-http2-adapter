ce1ad3aa8a78dc271fc08e9ccea06bff1ed8a034
Cory Dyer <cjdyer0@gmail.com>
2025-06-02 18:54:05 +1000

    test: add ts module tests

---
+++ test/module/ts/index.ts
@@ -0,0 +1,7 @@
+import assert from 'assert';
+import createHTTP2Adapter from "axios-http2-adapter";
+
+assert.strictEqual(typeof createHTTP2Adapter, "function");
+
+const adapter = createHTTP2Adapter();
+assert.strictEqual(typeof adapter, "function");
